ce3d255b0c45f7447ddcab469fea15808b5a1335
MicheleOM <115908319+MicheleOM@users.noreply.github.com>
2024-08-22 22:29:42 -0300

    inclusao de exercicio da pag 36

---
+++ Entendendo Algoritmos - Aditya Y. Bhargava/pag36-exercicios-bigO.py
@@ -0,0 +1,6 @@
+# pag.36 exercícios big O
+
+# 1.3 - pressupondo que a lista telefonica é ordenada por nome, usa-se O(log n)
+# 1.4 - partindo do pressuposto da questão anterior, os numeros de telefone estariam desorganizado, restando portanto utilizar O(n)
+# 1.5 - para ler o número de cada pessoa, também utiliza-se tempo linear O(n)
+# 1.6 - Com a lista ordenada alfabeticamente, é necessário ler os números de todos com a letra A
