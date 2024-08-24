42a8c5b0632036bc2c53e67197874d62a1314bef
MicheleOM <115908319+MicheleOM@users.noreply.github.com>
2024-08-24 12:29:42 -0300

    inserçao de exercicio pag 46 e resposta

---
+++ Entendendo Algoritmos - Aditya Y. Bhargava/pag46-exercicios-arrays-e-listas.py
@@ -0,0 +1,5 @@
+# pag.46 exercícios arrays e listas
+
+#2.1 Suponha que você esteja criando um aplicativo para acompanhar as suas finanças. Todos os dias você anotara tudo o que gastou e onde gastou. No final do mês, você devera revisar os seus gastos e resumir o quanto gastou. Logo, você terá um monte de inserções e poucas leituras. Você deverá usar um array ou uma lista para implementar este aplicativo?
+
+#Lista será mais adequada para o propósito, pois, permite inserções eficientes, o que é essencial para registrar gastos diários. Além disso, como você fará poucas leituras, percorrer a lista no final do mês para revisar os gastos será suficiente.
