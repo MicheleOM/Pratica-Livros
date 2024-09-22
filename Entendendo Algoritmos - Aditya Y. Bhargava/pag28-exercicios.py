203b063591e639df737ac12706b00c1c32251643
MicheleOM <115908319+MicheleOM@users.noreply.github.com>
2024-09-22 20:52:11 -0300

    ajuste nome pag49 e inclusão enunciado perguntas

--- Entendendo Algoritmos - Aditya Y. Bhargava/pag28-exercicios.py
+++ Entendendo Algoritmos - Aditya Y. Bhargava/pag28-exercicios.py
@@ -2,20 +2,22 @@
 
 import math
 
+# Suponha que você tenha uma lista com 128 nomes e esteja fazendo uma pesquisa binária. Qual seria o número máximo de etapas que você levaria para encontrar o nome desejado?
+
 # Número de elementos na lista
 n = 128
 
 # Máximo de etapas
-maxEtapas = math.ceil(math.log2(n))
+maxEtapas = math.log2(n)
 print(maxEtapas)
 
 
-
+# Suponha que você duplique o tamanho da lista. Qual seria o número máximo de etapas agora?
 
 # Duplicando o tamanho da lista
 nDuplicado = n * 2
 
 # Máximo de etapas para lista duplicada
-maxEtapasDuplicado = math.ceil(math.log2(nDuplicado))
+maxEtapasDuplicado = math.log2(nDuplicado)
 print(maxEtapasDuplicado)
 
